37ad99c79b76021b8f2f4b8974e8e6560e0e7dea
Daniel Fernandes <danielalmfernandes@hotmail.com>
2022-04-03 10:23:52 +0100

    fist commit

--- src/pt/isec/pa/apoio_poe/Main.java
+++ src/pt/isec/pa/apoio_poe/Main.java
@@ -3,6 +3,6 @@ package pt.isec.pa.apoio_poe;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("asdsa");
+        System.out.println("asdsa");//asdsa
     }
 }
