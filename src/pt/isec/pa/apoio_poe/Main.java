03ab7abd2be32067e602b62c7a4999042e728d1c
Daniel Fernandes <danielalmfernandes@hotmail.com>
2022-04-03 16:43:48 +0100

    Diagrama de Classes da Maquina de Estados

--- src/pt/isec/pa/apoio_poe/Main.java
+++ src/pt/isec/pa/apoio_poe/Main.java
@@ -3,6 +3,7 @@ package pt.isec.pa.apoio_poe;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("asdsa");//asdsa
+        System.out.println("asdsa");//++
+        // asdsa
     }
 }
